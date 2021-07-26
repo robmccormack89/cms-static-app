aaa92caa23de67fac56f99790a3cd2c797735a68
robmccormack89 <robertmccormack147@gmail.com>
2021-07-27 00:00:58 +0100

    more uncomment

--- app/models/QueryModel.php
+++ app/models/QueryModel.php
@@ -175,11 +175,11 @@ class QueryModel {
       if($this->dayParam()){
         $new_args_array['date']['day'] = $this->dayParam();
       }
-      // if($this->yearParam() {
-      //   $new_args_array['date'][] = array(
-      //     'year' => $this->yearParam()
-      //   );
-      // }
+      if($this->yearParam() {
+        $new_args_array['date'][] = array(
+          'year' => $this->yearParam()
+        );
+      }
     }
     
     if($this->nameParam()){
